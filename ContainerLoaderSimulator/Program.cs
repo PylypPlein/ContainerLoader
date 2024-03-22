91ea49d50cdefc6858760e6dd362feb7b65eee0d
s24675 <s24675@pjwstk.edu.pl>
2024-03-22 09:05:47 +0100

    Realizacja menu glownego aplikacji

--- ContainerLoaderSimulator/Program.cs
+++ ContainerLoaderSimulator/Program.cs
@@ -2,4 +2,49 @@
 /*
  * @author s24675
  */
-Console.WriteLine("Hello, ContainerLoader!");
+
+public class Program
+{
+ public static void Main(String[] args)
+ {
+  int choose;
+  do
+  {
+   ShowMainMenu();
+   choose = Convert.ToInt32(Console.ReadLine());
+   if (choose == 0)
+   {
+    break;
+   }
+   switch (choose)
+   {
+    case 1:
+     //menu tworzenia kontenera
+     break;
+    case 2:
+     //menu wyboru oraz zaladowania kontenera
+     break;
+    default:
+     Console.WriteLine("Wybierz dostępną opcje!");
+     break;
+   }
+   {
+    
+   }
+  } while (choose != 0);
+  ExitMessage();
+ }
+
+ private static void ShowMainMenu()
+ {
+  Console.WriteLine("Container loader app v1.0");
+  Console.WriteLine("1 - stwórz kontener");
+  Console.WriteLine("2 - zaladuj kontener");
+  Console.WriteLine("0 - exit");
+ }
+
+ private static void ExitMessage()
+ {
+  Console.WriteLine("Do widzenia!");
+ }
+}
